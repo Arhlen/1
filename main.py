5931cc0286e75aca10d245e2124272c72fe63d6d
Arhlen <elarkhipova@yandex.ru>
2024-06-07 15:26:11 +0300

    Unit

--- main.py
+++ main.py
@@ -1,60 +1,73 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def get_pas_count(lines, min_age):
-    total = 0
-    saved = 0
-
+def get_pas_count(lines, level_age_30):
+    total = [0, 0, 0]
+    saved = [0, 0, 0]
     for line in lines:
         data = line.split(',')
-        age = data[6]
-        if age != '' and min_age <= float(age) <= min_age + 30:
-            total += 1
-            if data[1] == "1":
-                saved += 1
-    Val1 = saved / total * 100
-    Val2 = (total - saved) / total * 100
-    return Val1, Val2
+        if data [6] != '':
+            age = float(data[6])
+            saved[int(age/30)] += int(data[1])
+            total[int(age/30)] += 1
+    return saved[level_age_30] / total[level_age_30] * 100, \
+        (total[level_age_30] - saved[level_age_30]) / total[level_age_30] * 100
 
 with open('data.csv') as file:
-    text = file.readlines()
+        text = file.readlines()
 
-    print(get_pas_count(text[1:], min_age=0))
+print(get_pas_count(text[1:], 0))
+print(get_pas_count(text[1:], 30))
+print(get_pas_count(text[1:], 60))
 
-def get_pas_count(lines, min_age):
-    total = 0
-    saved = 0
 
-    for line in lines:
-        data = line.split(',')
-        age = data[6]
-        if age != '' and min_age <= float(age) <= min_age + 30:
-            total += 1
-            if data[1] == "1":
-                saved += 1
-    return saved / total * 100, (total - saved) / total * 100
+#        if age != '' and min_age <= float(age) <= min_age + 30:
+#            total += 1
+#            if data[1] == "1":
+#                saved += 1
+#    Val1 = saved / total * 100
+#    Val2 = (total - saved) / total * 100
+#    return Val1, Val2
 
-with open('data.csv') as file:
-    text = file.readlines()
-    print(get_pas_count(text[1:], 30))
+#with open('data.csv') as file:
+#    text = file.readlines()
 
-def get_pas_count(lines, min_age):
-    total = 0
-    saved = 0
+#    print(get_pas_count(text[1:], min_age=0))
 
-    for line in lines:
-        data = line.split(',')
-        age = data[6]
-        if age != '' and min_age <= float(age):
-            total += 1
-            if data[1] == "1":
-                saved += 1
-    return saved / total * 100, (total - saved) / total * 100
+#def get_pas_count(lines, min_age):
+#    total = 0
+#    saved = 0
 
-with open('data.csv') as file:
-    text = file.readlines()
+#    for line in lines:
+#        data = line.split(',')
+#        age = data[6]
+#        if age != '' and min_age <= float(age) <= min_age + 30:
+#            total += 1
+#            if data[1] == "1":
+#                saved += 1
+#    return saved / total * 100, (total - saved) / total * 100
 
-    print(get_pas_count(text[1:], 60))
+#with open('data.csv') as file:
+#    text = file.readlines()
+#    print(get_pas_count(text[1:], 30))
+
+#def get_pas_count(lines, min_age):
+#    total = 0
+#    saved = 0
+
+#    for line in lines:
+#        data = line.split(',')
+#        age = data[6]
+#        if age != '' and min_age <= float(age):
+#            total += 1
+#            if data[1] == "1":
+#                saved += 1
+#    return saved / total * 100, (total - saved) / total * 100
+
+#with open('data.csv') as file:
+#    text = file.readlines()
+
+#    print(get_pas_count(text[1:], 60))
 
 columns = ['до 30 лет', 'от 30 до 60 лет', 'старше 60 лет']
 st.title('Пассажиры Титаника')
@@ -65,16 +78,12 @@ index = columns.index(choice)
 with open('data.csv') as file:
     lines = file.readlines()[1:]
 
-[min_age, val] = get_pas_count(lines, index)
-data = {'Возраст': ['Выжившие', 'Погибшие'], 'Доля,%': [min_age, val]}
-st.table(data)
-
-x = [min_age]
-y = ['Val1', 'Val2']
-fig = plt.figure(figsize=[10, 5])
-
-plt.xlabel('Возраст пассажиров')
-plt.ylabel('Доля выживших пассажиров')
-plt.title('Диаграмма')
-plt.bar(x, y)
-st.pyplot(fig)
+#[min_age, val] = get_pas_count(lines, index)
+#data = {'Возраст': ['Выжившие', 'Погибшие'], 'Доля,%': [min_age, val]}
+#st.table(data)
+#fig = plt.figure(figsize=[10, 5])
+#plt.xlabel('Возраст пассажиров')
+#plt.ylabel('Доля выживших пассажиров')
+#plt.title('Диаграмма')
+#plt.bar(x, y)
+#st.pyplot(fig)
