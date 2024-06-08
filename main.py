d74628f0d01e12519d6125ec7fb180d149125bcc
Arhlen <elarkhipova@yandex.ru>
2024-06-08 07:11:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,39 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def get_pas_count(lines, level_age_30):
-    total = [0, 0, 0]
-    saved = [0, 0, 0]
+def get_pas_count(lines, min_age):
+    total = 0
+    saved = 0
+
     for line in lines:
         data = line.split(',')
-        if data [6] != '':
-            age = float(data[6])
-            saved[int(age/30)] += int(data[1])
-            total[int(age/30)] += 1
-    return saved[level_age_30] / total[level_age_30] * 100, \
-        (total[level_age_30] - saved[level_age_30]) / total[level_age_30] * 100
+        age = data[6]
+        if age != '' and min_age <= float(age) < min_age + 30:
+            total += 1
+            if data[1] == "1":
+               saved += 1
+    return round(saved / total * 100, 2), \
+        round((total - saved) / total * 100, 2)
+    
+def var11_main():
+    columns = ['до 30 лет', 'от 30 до 60 лет', 'старше 60 лет']
+    st.title('Пассажиры Титаника')
+    st.write('Для просмотра информации о доле выживших/погибших пассажиров выберите возраст:')
+    choice = st.selectbox('Выберите возраст:', columns)
+    index = columns.index(choice)
 
-with open('data.csv') as file:
-        text = file.readlines()
+    with open('data.csv') as file:
+        lines = file.readlines()[1:]
 
-print(get_pas_count(text[1:], 0))
-print(get_pas_count(text[1:], 30))
-print(get_pas_count(text[1:], 60))
+    min_age, val = get_pas_count(lines, index * 30)
+    data = {'Возраст': ['Выжившие', 'Погибшие'], 'Доля,%': [min_age, val]}
+    st.table(data)
 
+    fig = plt.figure(figsize=(7, 4))   
+    data = {f'Доля выживших: {min_age}%':min_age, f'Доля погибших: {val}%': val}
+    labels = list(data.keys())
+    sizes = list(data.values())
+    colors = ['PaleGreen', 'MediumSlateBlue']
+    plt.pie(sizes, labels=labels, colors=colors)
+    st.pyplot(fig)
 
-#        if age != '' and min_age <= float(age) <= min_age + 30:
-#            total += 1
-#            if data[1] == "1":
-#                saved += 1
-#    Val1 = saved / total * 100
-#    Val2 = (total - saved) / total * 100
-#    return Val1, Val2
-
-#with open('data.csv') as file:
-#    text = file.readlines()
-
-#    print(get_pas_count(text[1:], min_age=0))
-
-#def get_pas_count(lines, min_age):
-#    total = 0
-#    saved = 0
-
-#    for line in lines:
-#        data = line.split(',')
-#        age = data[6]
-#        if age != '' and min_age <= float(age) <= min_age + 30:
-#            total += 1
-#            if data[1] == "1":
-#                saved += 1
-#    return saved / total * 100, (total - saved) / total * 100
-
-#with open('data.csv') as file:
-#    text = file.readlines()
-#    print(get_pas_count(text[1:], 30))
-
-#def get_pas_count(lines, min_age):
-#    total = 0
-#    saved = 0
-
-#    for line in lines:
-#        data = line.split(',')
-#        age = data[6]
-#        if age != '' and min_age <= float(age):
-#            total += 1
-#            if data[1] == "1":
-#                saved += 1
-#    return saved / total * 100, (total - saved) / total * 100
-
-#with open('data.csv') as file:
-#    text = file.readlines()
-
-#    print(get_pas_count(text[1:], 60))
-
-columns = ['до 30 лет', 'от 30 до 60 лет', 'старше 60 лет']
-st.title('Пассажиры Титаника')
-st.subheader('Вычислить долю погибших и спасенных пассажиров, указав возрастную категорию из списка - «молодой» (до 30 лет), «среднего возраста» (от 30 до 60) и «старый» (более 60 лет)')
-choice = st.selectbox('Для просмотра информации о доле выживших/погибших пассажиров выберите возраст:', columns)
-index = columns.index(choice)
-
-with open('data.csv') as file:
-    lines = file.readlines()[1:]
-
-#[min_age, val] = get_pas_count(lines, index)
-#data = {'Возраст': ['Выжившие', 'Погибшие'], 'Доля,%': [min_age, val]}
-#st.table(data)
-#fig = plt.figure(figsize=[10, 5])
-#plt.xlabel('Возраст пассажиров')
-#plt.ylabel('Доля выживших пассажиров')
-#plt.title('Диаграмма')
-#plt.bar(x, y)
-#st.pyplot(fig)
